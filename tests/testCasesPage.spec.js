363aa866f2b1fb0d7dc2d8c49356d0b2ceb1e459
Dragan <gale_vlae@yahoo.com>
2025-05-31 12:17:19 +0200

    added parallel execution of the tests in the same spec

--- tests/testCasesPage.spec.js
+++ tests/testCasesPage.spec.js
@@ -6,38 +6,40 @@ test.beforeEach(async ({ testCasesClass }) => {
   await testCasesClass.navigateToTestCases();
 });
 
-test(
-  "Verify all of the test cases are displayed on the page",
-  { tag: ["@testCasesPage", "@e2e"] },
-  async ({ page, testCasesClass }) => {
-    expect(page.url()).toContain("test_cases");
+test.describe.parallel("Test Cases tests group", () => {
+  test(
+    "Verify all of the test cases are displayed on the page",
+    { tag: ["@testCasesPage", "@e2e"] },
+    async ({ page, testCasesClass }) => {
+      expect(page.url()).toContain("test_cases");
 
-    const testCasesOnPage = await testCasesClass.getTestCasesList();
+      const testCasesOnPage = await testCasesClass.getTestCasesList();
 
-    testCasesList.testCases.forEach((testCase) => {
-      expect(testCasesOnPage).toContain(testCase);
-      logger.info(`Checking: ${testCase}`);
-    });
-  }
-);
-
-test(
-  "Verify that each Test Case can be expanded",
-  { tag: "@testCasesPage" },
-  async ({ testCasesClass }) => {
-    const count = await testCasesClass.testCasesList.count();
-
-    for (let i = 0; i < count; i++) {
-      const testCaseElement = testCasesClass.testCasesList.nth(i);
-      const text = await testCaseElement.innerText();
-
-      logger.info(`Clicking accordion: ${text}`);
-      await testCaseElement.click();
-
-      const stepCount = await testCasesClass.testStepsList.count();
-      expect(stepCount).toBeGreaterThan(0);
-
-      await testCaseElement.click();
+      testCasesList.testCases.forEach((testCase) => {
+        expect(testCasesOnPage).toContain(testCase);
+        logger.info(`Checking: ${testCase}`);
+      });
     }
-  }
-);
+  );
+
+  test(
+    "Verify that each Test Case can be expanded",
+    { tag: "@testCasesPage" },
+    async ({ testCasesClass }) => {
+      const count = await testCasesClass.testCasesList.count();
+
+      for (let i = 0; i < count; i++) {
+        const testCaseElement = testCasesClass.testCasesList.nth(i);
+        const text = await testCaseElement.innerText();
+
+        logger.info(`Clicking accordion: ${text}`);
+        await testCaseElement.click();
+
+        const stepCount = await testCasesClass.testStepsList.count();
+        expect(stepCount).toBeGreaterThan(0);
+
+        await testCaseElement.click();
+      }
+    }
+  );
+});
