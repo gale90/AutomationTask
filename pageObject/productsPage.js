8c668355bc864a54720b2ddec10921a0d3ea1dcd
Dragan <gale_vlae@yahoo.com>
2025-06-04 15:53:32 +0200

    search product test added

--- pageObject/productsPage.js
+++ pageObject/productsPage.js
@@ -1,21 +1,50 @@
-import { BasePage } from './basePage.js';
+import { BasePage } from "./basePage.js";
 
 export class Products extends BasePage {
   constructor(page) {
     super(page);
 
     this.page = page;
-    this.viewProducts = page.locator('div.choose li');
-    this.productNameTxt = page.locator('div.product-information > h2');
-    this.productCategoryTxt = page.locator('div.product-information > p:has-text("Category:")');
-    this.productPriceTxt = page.locator('div.product-information > span > span');
-    this.productAvailabilityTxt = page.locator('div.product-information > p b:has-text("Availability:")');
-    this.productConditionTxt = page.locator('div.product-information > p b:has-text("Condition:")');
-    this.productBrandTxt = page.locator('div.product-information > p b:has-text("Brand:")');
-
+    this.viewProducts = page.locator("div.choose li");
+    this.productNameTxt = page.locator("div.product-information > h2");
+    this.productCategoryTxt = page.locator(
+      'div.product-information > p:has-text("Category:")'
+    );
+    this.productPriceTxt = page.locator(
+      "div.product-information > span > span"
+    );
+    this.productAvailabilityTxt = page.locator(
+      'div.product-information > p b:has-text("Availability:")'
+    );
+    this.productConditionTxt = page.locator(
+      'div.product-information > p b:has-text("Condition:")'
+    );
+    this.productBrandTxt = page.locator(
+      'div.product-information > p b:has-text("Brand:")'
+    );
+    this.searchField = page.locator("input#search_product");
+    this.searchBtn = page.locator("button#submit_search");
+    this.searchedProductNamesList = page.locator("div.productinfo > p");
   }
 
   async clickOnFirstProduct() {
     await this.viewProducts.first().click();
+  }
+
+  async enterProduct(product) {
+    await this.searchField.type(product);
+  }
+
+  async clickOnSearch() {
+    await this.searchBtn.click();
+  }
+
+  async searchProduct(product) {
+    await this.enterProduct(product);
+    await this.clickOnSearch();
+  }
+
+  async getProductsList() {
+    return await this.searchedProductNamesList.allTextContents();
   }
 }
--- tests/products.spec.js
+++ tests/products.spec.js
@@ -1,22 +1,38 @@
 import { test, expect } from "../fixtures/fixtures.js";
 import logger from "../utils/logger.js";
 
-test(
-  "Verify that a user can view a product",
-  { tag: ["@productsPage", "@e2e"] },
-  async ({ page, productsClass }, testInfo) => {
-    logger.info(`Running test: ${testInfo.title}`);
-    await page.goto("/");
-    await productsClass.navigateToProducts();
-    expect(page.url()).toContain("products");
-    await productsClass.clickOnFirstProduct();
-    expect(page.url()).toContain("product_details");
+test.beforeEach(async ({ page, productsClass }, testInfo) => {
+  logger.info(`Running test: ${testInfo.title}`);
+  await page.goto("/");
+  await productsClass.navigateToProducts();
+  expect(page.url()).toContain("products");
+});
 
-    await expect(productsClass.productNameTxt).toBeVisible();
-    await expect(productsClass.productCategoryTxt).toBeVisible();
-    await expect(productsClass.productPriceTxt).toBeVisible();
-    await expect(productsClass.productAvailabilityTxt).toBeVisible();
-    await expect(productsClass.productConditionTxt).toBeVisible();
-    await expect(productsClass.productBrandTxt).toBeVisible();
-  }
-);
+test.describe("Products tests group", () => {
+  test(
+    "Verify that a user can view a product",
+    { tag: ["@productsPage", "@e2e"] },
+    async ({ page, productsClass }) => {
+      await productsClass.clickOnFirstProduct();
+      expect(page.url()).toContain("product_details");
+      await expect.soft(productsClass.productNameTxt).toBeVisible();
+      await expect.soft(productsClass.productCategoryTxt).toBeVisible();
+      await expect.soft(productsClass.productPriceTxt).toBeVisible();
+      await expect.soft(productsClass.productAvailabilityTxt).toBeVisible();
+      await expect.soft(productsClass.productConditionTxt).toBeVisible();
+      await expect.soft(productsClass.productBrandTxt).toBeVisible();
+    }
+  );
+
+  test(
+    "Verify that a user can search for a product",
+    { tag: ["@productsPage", "@e2e"] },
+    async ({ page, productsClass }) => {
+      await productsClass.searchProduct("jeans");
+      const products = await productsClass.getProductsList();
+      for (const productText of products) {
+        expect(productText.toLowerCase()).toContain("jeans");
+      }
+    }
+  );
+});
